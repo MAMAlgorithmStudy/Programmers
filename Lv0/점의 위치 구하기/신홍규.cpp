5e8ffb1f7ac506f33820f52d4b8e57ee32c8cd07
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-08-18 00:38:09 +0900

    Create 신홍규.cpp

---
+++ Lv0/점의 위치 구하기/신홍규.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    if(dot[0] > 0){
+        if(dot[1] > 0) return 1;
+        else return 4;
+    }
+    else{
+        if(dot[1] > 0) return 2;
+        else return 3;
+    }
+}
