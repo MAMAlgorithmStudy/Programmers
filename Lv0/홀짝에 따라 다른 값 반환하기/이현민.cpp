4e4a18e52f3e12578e5628a191d1c86ec56adf82
nimnoeyheel <hyeonmin1012@gmail.com>
2024-08-24 21:12:46 +0900

    Create 이현민.cpp

---
+++ Lv0/홀짝에 따라 다른 값 반환하기/이현민.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    if(n % 2 == 1)
+    {
+        for(int i = 1; i <= n; i += 2)
+        {
+            answer += i;
+        }
+    }
+    else if(n % 2 == 0)
+    {
+        for(int i = 2; i <= n; i += 2)
+        {
+            answer += i*i;
+        }
+    }
+    return answer;
+}
