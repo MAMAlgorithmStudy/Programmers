078687fdb8c4a189d5d1aca7432ef9224d0e81bc
nimnoeyheel <hyeonmin1012@gmail.com>
2024-08-30 09:41:16 +0900

    Create 이현민.cpp

---
+++ Lv0/정수 찾기/이현민.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    for(int i = 0; i < num_list.size(); ++i)
+    {
+        if(n == num_list[i]) return 1;
+    }
+    return 0;
+}
