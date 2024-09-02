13cd8bf2978b2f50eedd8cba4d9f540182bdcb08
Subin <80036437+so0oppy@users.noreply.github.com>
2024-09-02 14:53:50 +0900

    Create 수빈.cpp

---
+++ Lv0/0 떼기/수빈.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    
+    int n = stoi(n_str);
+    answer = to_string(n);
+    
+    return answer;
+}
