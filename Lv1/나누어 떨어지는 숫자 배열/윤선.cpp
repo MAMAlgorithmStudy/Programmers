51bbab06c3dadaf713ff415534c644da5b6cf7d7
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-09-03 13:42:41 +0900

    Create 윤선.cpp
    https://school.programmers.co.kr/learn/courses/30/lessons/12910

---
+++ Lv1/나누어 떨어지는 숫자 배열/윤선.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(arr[i]%divisor == 0)
+            answer.push_back(arr[i]);
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    if(answer.empty())
+        answer.push_back(-1);
+    
+    return answer;
+}
