9ae9e82e8ba5ad4c392489e38ea10aaf82bee7d8
SUHYEOK JANG <kr.suhyeok@gmail.com>
2024-09-10 09:04:35 +0900

    Update 장수혁.cpp

--- Lv1/음양 더하기/장수혁.cpp
+++ Lv1/음양 더하기/장수혁.cpp
@@ -8,7 +8,6 @@ int solution(vector<int> absolutes, vector<bool> signs)
 {
     int answer = 0; // 실제 정수들의 합 저장
 
-    // absolutes와 signs의 크기는 같으며, 이 크기만큼 반복합니다.
     for(int i = 0; i < absolutes.size(); i++) 
     {
         // signs[i]가 false일 때
