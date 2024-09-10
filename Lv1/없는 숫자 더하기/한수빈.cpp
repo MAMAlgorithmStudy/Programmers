10ea3a4df9c02275bc54e9dba4ea5c3f7872615f
Subin <80036437+so0oppy@users.noreply.github.com>
2024-09-10 09:11:13 +0900

    Create 한수빈.cpp

---
+++ Lv1/없는 숫자 더하기/한수빈.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    
+    for(int i=0; i<10; i++)
+    {
+        if(find(numbers.begin(), numbers.end(), i) == numbers.end())
+            answer += i;
+    }
+    
+    return answer;
+}
