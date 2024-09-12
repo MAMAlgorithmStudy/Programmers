e2cd5ac996171ac2db153ca40b59103bb6b7d10d
Subin <80036437+so0oppy@users.noreply.github.com>
2024-09-12 09:25:45 +0900

    Create 한수빈.cpp

---
+++ Lv1/같은 숫자는 싫어/한수빈.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+
+    answer.push_back(arr[0]);
+    
+    for(int i=1; i<arr.size(); i++)
+    {
+        if(arr[i-1] != arr[i])
+            answer.push_back(arr[i]);
+    }
+
+    return answer;
+}
