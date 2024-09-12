9b3d699d1e3fe194d7bdf057a2cd94816a6c656e
nimnoeyheel <hyeonmin1012@gmail.com>
2024-09-12 09:11:23 +0900

    Create 이현민.cpp

---
+++ Lv1/x만큼 간격이 있는 n개의 숫자/이현민.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    int origin_x = x;
+    for(int i = 0; i < n; ++i)
+    {
+        answer.push_back((long long)x);
+        x += origin_x;
+    }
+    return answer;
+}
