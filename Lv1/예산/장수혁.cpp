f659b032aeacc98acc74f978de507b5a59e9c219
SUHYEOK JANG <kr.suhyeok@gmail.com>
2024-09-12 09:25:47 +0900

    Update 장수혁.cpp

--- Lv1/예산/장수혁.cpp
+++ Lv1/예산/장수혁.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+int solution(vector<int> d, int budget) 
+{
+    int answer = 0; 
+
+    sort(d.begin(), d.end());
+
+    for(int i = 0; i < d.size(); i++)
+    {
+        if(d[i] > budget) 
+        break;
+
+        budget -= d[i];
+    
+        answer++;
+    }
+  
+    return answer;
+}
