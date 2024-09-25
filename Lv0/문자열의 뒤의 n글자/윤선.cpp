eff353169657f6847f45338264a6467d63ccd1d0
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-09-25 13:20:47 +0900

    Create 윤선.cpp

---
+++ Lv0/문자열의 뒤의 n글자/윤선.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    for (int i = my_string.size() - n; i < my_string.size(); ++i)
+    {
+        answer.push_back(my_string[i]);
+    }
+    return answer;
+}
