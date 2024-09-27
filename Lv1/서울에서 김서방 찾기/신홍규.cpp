0717a41086725077aea1b6e4ebb6fd03815bfb50
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-09-27 14:21:52 +0900

    Create 신홍규.cpp

---
+++ Lv1/서울에서 김서방 찾기/신홍규.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    answer += "김서방은 ";
+    for(int i = 0; i< seoul.size();i++){
+        if(seoul[i] == "Kim"){
+            answer += to_string(i);
+        }
+    }
+    answer += "에 있다";
+    return answer;
+}
