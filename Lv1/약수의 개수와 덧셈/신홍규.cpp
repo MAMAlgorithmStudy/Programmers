4c724a4e5c060028ad38f037be56724a7f4b9f1e
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-09-27 14:32:17 +0900

    Create 신홍규.cpp

---
+++ Lv1/약수의 개수와 덧셈/신홍규.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int left, int right) {
+    int answer = 0;
+    int cnt = 0;
+    for(int i = left; i<=right; i++){
+        cnt = 0;
+        for(int j =1; j<=i; j++){
+            if(i%j==0){
+                cnt++;
+            }
+        }
+        if(cnt % 2 == 0){
+            answer += i;
+        }
+        else {
+            answer -= i;
+        }
+    }
+    return answer;
+}
