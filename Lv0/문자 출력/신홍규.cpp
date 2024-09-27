b2f8e65f879d79b985f3b01f9834d8035b0edbc1
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-09-27 14:41:10 +0900

    Create 신홍규.cpp

---
+++ Lv0/문자 출력/신홍규.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string message = "
+Let's go!
+";
+
+    cout << "3
+\n
+2
+\n
+1" << endl;
+    cout << message << endl;
+
+    return 0;
+}
