5035e31b6f146804d572108165ab83c7ea010b54
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-10-04 10:13:21 +0900

    Create 윤선.cpp

---
+++ Lv0/꼬리 문자열/윤선.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+
+    for(int i = 0; i < str_list.size(); i++)
+    {
+        if (str_list[i].find(ex) == string::npos)
+        {
+            answer += str_list[i];
+        }   
+    }
+    return answer;
+}
