768c6143038e55fe03256dd1fe494a4c4adc216b
jyunseon81 <102278574+jyunseon81@users.noreply.github.com>
2024-10-10 10:02:41 +0900

    Create 윤선.cpp

---
+++ Lv0/배열의 원소 삭제하기/윤선.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<int> delete_list) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if(find(delete_list.begin(), delete_list.end(), arr[i]) == delete_list.end()) 
+            answer.push_back(arr[i]);
+    }
+    return answer;
+}
