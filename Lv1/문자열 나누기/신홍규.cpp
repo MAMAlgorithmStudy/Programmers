614af5406923ca6b107728471d767b0447350e8e
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2024-11-09 15:37:46 +0900

    Create 신홍규.cpp

---
+++ Lv1/문자열 나누기/신홍규.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string divideString(string s)
+{
+    int x = s[0];
+    int notX = 0;
+    int isX = 0;
+    string temp = "";
+    
+    for(int i = 0; i <s.length(); i++)
+    {
+        if(isX != 0 && isX == notX)
+        {
+            temp+=s[i];
+        }
+        else
+        {
+            if(s[i] == x)
+            {
+                isX++;
+            }
+            else
+            {
+                notX++;
+            }
+        }
+    }
+    return temp;
+}
+
+int solution(string s) {
+    int answer = 0;
+    string s_temp = s;
+    while(s_temp.length() >= 1)
+    {
+        answer++;
+        s_temp = divideString(s_temp);
+    }
+    return answer;
+}
